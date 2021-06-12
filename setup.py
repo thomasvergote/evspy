3bbc2f2dc55a04acf74bb4a1459d9b150e1567f6
Thomas Vergote <thomasvergote@gmail.com>
2021-06-12 10:49:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+# load the README file and use it as the long_description for PyPI
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
 setup(name='evspy',
-      version='0.1.0',
-      description='Modelling of creep and swelling during unloading',
+      version='0.1.2',
+      description='Modelling of consolidation with creep and swelling of soils',
       url='http://github.com/thomasvergote/evspy',
       author='Thomas Vergote',
-      author_email='thomasvergote@u.nus.edu',
+      author_email='thomas@inferensics.be',
       license='GPL v3',
       packages=['evspy'],
+      keywords=['engineering', 'geotechnical'],
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+      ],
       zip_safe=False)
