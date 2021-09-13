e5ff5c20200d52c5129533f19c04952a229de2c8
Thomas Vergote <thomasvergote@gmail.com>
2021-09-13 16:52:52 +0200

    fix swelling law relations

--- evspy/coupled_fdm.py
+++ evspy/coupled_fdm.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pynverse import inversefunc
 import tqdm
+import logging
 
 from evspy.consolidation import Consol_Terzaghi_Uavg_vertical
 from evspy.viscous_swelling import swelling_law
@@ -15,13 +16,18 @@ class distorted_isotache_model(base_isotache_model):
     base_isotache_model : class
         Base class for isotache models
     '''
-    def initialize_decoupled(self,sigma0,e_init,erateinits):
-        self.time_step={}
+    def initialize_decoupled(self,erateinits):
+        self.time_step_start={}
+        self.time_step_end={}
         self.time=np.array([0])
-        self.sigma=np.array([sigma0])
-        self.e[:]=e_init
+        self.sigma=np.array([self.sigma0])
+        self.e[:]=self.e_init
         self.initialize_reference_curve()
         self.sigpref[0] = self.get_sigpref(self.sigma[0:1],self.e[0:1])
+        if self.sigpref[0]<self.sigma0:
+            self.e[0] = self.eNC[self.sigrangeNC>self.sigma0][1]
+            logging.warning(f'Initial effective stress is too high for given initial void ratio; reducing to einit = {self.e[0]}')
+            self.sigpref[0] = self.get_sigpref(self.sigma[0:1],self.e[0:1])
         self.OCR_real[0]=self.sigpref[0]/self.sigma[0]
         self.OCR[0]=self.sigpref[0]/self.sigma[0]
         #print(sigma[0],e[0],beta2,beta3,Cc,Cr,CalphaNC,sigpref[0],e0,isotache,power_law,ref_func)
@@ -36,7 +42,7 @@ class distorted_isotache_model(base_isotache_model):
         
         #for t in tqdm.tqdm(range(1,dimt)):
 
-    def initiate_new_load_step(self,load_step,t,running):
+    def initiate_new_load_step(self,load_step,t,running,reset_minimum_swelling_rate):
         next_load_step=False
         if self.load.type[load_step]=='IL':
             if (t>=len(self.time)-1): #(time[t]>=load.end_time[load_step])|
@@ -50,13 +56,13 @@ class distorted_isotache_model(base_isotache_model):
                 if self.sigma[t-1]<=self.load.load[load_step]:
                     next_load_step=True
             else:
-                if (self.time[t]-self.time_step[self.load.type[load_step-1]+str(load_step-1)]>=self.load.duration[load_step]):
+                if (self.time[t]-self.time_step_end[self.load.type[load_step-1]+str(load_step-1)]>=self.load.duration[load_step]):
                     next_load_step=True
         if next_load_step:
-            reset=0
+            reset_minimum_swelling_rate = 0
             if len(self.load.type)>load_step+1:
                 #print('next step '+str(load_step))
-                self.time_step[self.load.type[load_step]+str(load_step)]=self.time[t]
+                self.time_step_end[self.load.type[load_step]+str(load_step)]=self.time[t]
                 load_step+=1
                 if self.load.type[load_step]=='IL':
                     time_u,U=Consol_Terzaghi_Uavg_vertical(self.load.cv[load_step]/3600/24/365,self.H,targettime=self.load.duration[load_step],dimt=1000,constant_dt_time=1e6,dtmax=1e5)
@@ -65,9 +71,10 @@ class distorted_isotache_model(base_isotache_model):
                     self.sigma=np.append(self.sigma,self.sigma[-1]+1/U[-1]*(self.load.load[load_step]-self.sigma[-1])*np.clip(U,0,1))
                 elif self.load.type[load_step]=='CRS':
                     self.erate_crs = self.load.rate[load_step]
+                self.time_step_start[self.load.type[load_step]+str(load_step)]=self.time[t]
             else:
                 running=False
-        return next_load_step, running, load_step
+        return next_load_step, running, load_step, reset_minimum_swelling_rate
          
     def run_iterations(self):
         t = 0
@@ -85,9 +92,9 @@ class distorted_isotache_model(base_isotache_model):
         load_step = 0
         next_load_step=True
         running=True
-        reset=0
+        reset_minimum_swelling_rate = 0
         while (running)&(t<self.dimt-1):
-            next_load_step, running, load_step=self.initiate_new_load_step(load_step,t,running)
+            next_load_step, running, load_step, reset_minimum_swelling_rate=self.initiate_new_load_step(load_step,t,running, reset_minimum_swelling_rate)
             if running:
                 t+=1
             if self.load.type[load_step]=='CRS':
@@ -109,7 +116,9 @@ class distorted_isotache_model(base_isotache_model):
             self.OCRrate[t] = (self.OCR[t]-self.OCR[t-1])/dt
             
             sigmarate=(self.sigma[t-1]-self.sigma[t-2])/dt
-            self.erates[t],self.Calphahats_real[t-1],reset=swelling_law(self.erates[t-1],self.OCR[t],self.OCRrate[t],self.Calphahats_real[t-1],dt,self.CalphaCc*self.Cc,self.m1,self.b1,self.m2,self.b2,sigmarate,-self.erate_e[t],reset)
+            self.erates[t],self.Calphahats_real[t-1],reset_minimum_swelling_rate=swelling_law(self.erates[t-1],self.OCR[t],self.OCRrate[t],
+                                                                        self.Calphahats_real[t-1],dt,self.CalphaCc*self.Cc,
+                                                                        self.m1,self.b1,self.m2,self.b2,sigmarate,-self.erate_e[t],reset_minimum_swelling_rate)
             
             # Distortion occurs only during swelling and not during relaxation
             if ((self.sigma[t]<self.sigma[t-1]/1.000001)
@@ -139,6 +148,7 @@ class distorted_isotache_model(base_isotache_model):
             
             self.Calphahats_real[t]=self.CalphaCc*self.Cc*10**self.b1*(np.clip(self.OCR[t]-1,0.005,np.infty))**self.m1
 
+        self.time = self.time[:t]
         self.sigma = self.sigma[:t]
         self.e = self.e[:t]
         self.erate = self.erate[:t]
@@ -146,15 +156,15 @@ class distorted_isotache_model(base_isotache_model):
         self.sigpref = self.sigpref[:t]
         self.eratec = self.eratec[:t]
     
-    def plotting(self):
+    def plotting_stresspath(self,sigp):
         dforg,eUC_org,eUC0_org,eNC_org,sigrange_org,sigrangeNC=create_isotache(erateref = self.erateref, beta2 = self.beta2, 
                                                                                 beta3 = self.beta3,Cc = self.Cc,Cr = self.Cr,CalphaNC = self.CalphaNC,
-                                                                                sigp=(self.sigpref[0]), e0=self.e0,isotache=self.isotache, power_law=self.use_power_law, 
+                                                                                e0=self.e0,isotache=self.isotache, power_law=self.use_power_law, 
                                                                                 ref_func=self.ref_func, dsig=self.dsig, beta_nash=self.use_beta_nash,param_nash=self.param_nash)
         dforg=dforg.reset_index()
-        cs=make_contour(dforg,eUC_org,eUC0_org,eNC_org,sigrange_org,sigrangeNC,num=20,figsize=(8,5),vmax=-3)
+        cs=make_contour(dforg,eUC_org,eUC0_org,eNC_org,sigrange_org,sigrangeNC,num=20,figsize=(8,5),vmax=-3,colorbar=True)
         plt.plot(self.sigma[0],self.e[0],'o')
-        plt.plot(self.sigma,self.e,'-',lw=4)
+        plt.plot(self.sigma,self.e,'k-',lw=4)
         plt.ylim(np.min(self.e),np.max(self.e))
         plt.xlim(1,1000)
         #df,eUC,eUC0,eNC,sigrange,sigrangeNC=create_isotache(beta2 = beta2, beta3 = beta3,Cc = Cc,Cr = Cr,CalphaNC = CalphaNC,sigp=sigpref[t-1],e0=e0,isotache=isotache,power_law=power_law,ref_func=ref_func,dsig=0.05,beta_nash=beta_nash)
@@ -166,6 +176,10 @@ class distorted_isotache_model(base_isotache_model):
     #print(load_step)
     #return time[:t],sigma[:t],e[:t],erate[:t],erate_e[:t],eratec[:t],erates[:t],Calphahats_real[:t],ee[:t],sigpref[:t],time_step,OCR[:t],OCR_real[:t],sigp_update[:t]
 
+    def plotting_timeseries(self,loadstep : str):
+        time = self.time[(self.time>self.time_step_start[loadstep])&(self.time<self.time_step_end[loadstep])]-self.time[(self.time>=self.time_step_start[loadstep])][0]
+        e = self.e[(self.time>self.time_step_start[loadstep])&(self.time<self.time_step_end[loadstep])]
+        plt.semilogx(time,e)
 
 def distorted_isotaches_coupled(load,sigma0,H,erateref = 1e-5,dimt=8000,erateinits=1e-10,Cc=0.5,Cr=0.5/5,CalphaNC=0.5*0.04,k1=-8,k2=2,kf='k1',Cv=7,isotache = False,power_law=False,ref_func='semilogx',beta2=3,beta3=16,e0=2.3,dsig=0.005,de=0.0002,plotting=True,m1=0.88,b1=-0.3,b2=-5.,m2=2.,beta_nash=False,param_nash=[0.023,0.33,13],Calpha_OCRref=True,dimx=20,OpenBottom=False,sig0=1,gamman=17,gammas=27,gammaw=10,strain_definition='e/(1+e)',kdep=True,largestrain=True):
     CalphaCc=CalphaNC/Cc
