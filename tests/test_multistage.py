3557f819c7a6f10073965a05e6389c4cad24434c
Thomas Vergote <thomasvergote@gmail.com>
2021-09-14 16:08:16 +0200

    improved testing

--- tests/test_multistage.py
+++ tests/test_multistage.py
@@ -8,7 +8,7 @@ def test_decoupled_consolidation_IL():
     load.add_load_step(duration = 1e8,load = 5,rate = 1e-8,type_test = 'IL',
                         cv=7)
     model = distorted_isotache_model(load,sigma0=1,H=0.1,dimt=1000,e_init=1)
-    model.initialize_decoupled(erateinits = 1e-10)
+    model.initialize_decoupled()
     model.run_iterations()
     assert model.sigma[-1] == 5
 
@@ -20,7 +20,7 @@ def test_decoupled_consolidation_CRS():
                         type_test = 'CRS',
                         cv=7)
     model = distorted_isotache_model(load,sigma0=1,H=0.1,dimt=1500,e_init=1)
-    model.initialize_decoupled(erateinits = 1e-10)
+    model.initialize_decoupled()
     model.run_iterations()
     assert np.round(model.sigma[-1],1) == 5
     assert np.round(np.log10(-model.erate[-1]),1) == -8
